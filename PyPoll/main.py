5a65aee15fabb23b3414dec8db011bab68168078
wemlaztdj <wemlhui@gmail.com>
2023-07-08 08:08:09 -0400

    Should I do it using csv?

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,11 +1,17 @@
 #pyPoll
 
-import pandas as pd
+#import pandas as pd
 
-df = pd.read_csv('PyPoll/Resources/election_data.csv')
-total_votes = df['Ballot ID'].count()
-candidates = df['Candidate'].unique()
-print(total_votes)
-print(candidates)
-c_vote = df['Candidate'].value_counts(normalize=True)
-print(c_vote)
+#df = pd.read_csv('PyPoll/Resources/election_data.csv')
+#total_votes = df['Ballot ID'].count()
+#candidates = df['Candidate'].unique()
+#print(total_votes)
+#print(candidates)
+#c_vote = df['Candidate'].value_counts(normalize=True)
+#print(c_vote)
+
+import csv
+with open('PyPoll/Resources/election_data.csv', 'r') as f:
+  reader = csv.reader(f)
+  for row in reader:
+    print(row)
