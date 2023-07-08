6ad1a19cff379899356e0a08d9e3be7e869a7a22
wemlaztdj <wemlhui@gmail.com>
2023-07-07 23:15:38 -0400

    df.loc can get the date

--- PyBank/main.py
+++ PyBank/main.py
@@ -19,11 +19,13 @@ net_total = df['Profit/Losses'].sum()
 max_total = df['Profit/Losses'].max()
 min_total = df['Profit/Losses'].min()
 avg_total = round(df['Profit/Losses'].mean(),2)
+max_date = df.loc[df['Profit/Losses'].idxmax(),'Date']
+min_date = df.loc[df['Profit/Losses'].idxmin(),'Date']
 
 print('Financial Analysis')
 print('----------------------------')
 print('Total Months:',total_months)
 print('Total:',net_total)
 print('Average Change: $',avg_total)
-print('Greatest Increase in Profits: ',max_total)
-print('Greatest Decrease in Profits: ',min_total)
+print('Greatest Increase in Profits: ',max_date,max_total)
+print('Greatest Decrease in Profits: ',min_date,min_total)
