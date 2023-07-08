9c13f5cf3dfd462d3ff3d9c31ae13b7ce04a956e
wemlaztdj <wemlhui@gmail.com>
2023-07-08 00:01:15 -0400

    Need to work on PyPoll percentage

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,11 @@
 #pyPoll
+
+import pandas as pd
+
+df = pd.read_csv('PyPoll/Resources/election_data.csv')
+total_votes = df['Ballot ID'].count()
+candidates = df['Candidate'].unique()
+print(total_votes)
+print(candidates)
+c_vote = df['Candidate'].value_counts(normalize=True)
+print(c_vote)
