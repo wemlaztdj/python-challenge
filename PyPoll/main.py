9de53fda7a2a69747a1f93e62fcdc17144e0c224
wemlaztdj <wemlhui@gmail.com>
2023-07-08 21:34:04 -0400

    Analysis update

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,14 +1,20 @@
 #pyPoll
 
-#import pandas as pd
+# The total number of votes cast
+# A complete list of candidates who received votes
+# The percentage of votes each candidate won
+# The total number of votes each candidate won
+# The winner of the election based on popular vote
 
-#df = pd.read_csv('PyPoll/Resources/election_data.csv')
-#total_votes = df['Ballot ID'].count()
-#candidates = df['Candidate'].unique()
-#print(total_votes)
-#print(candidates)
-#c_vote = df['Candidate'].value_counts(normalize=True)
-#print(c_vote)
+
+# import pandas as pd
+# df = pd.read_csv('PyPoll/Resources/election_data.csv')
+# total_votes = df['Ballot ID'].count()
+# candidates = df['Candidate'].unique()
+# print(total_votes)
+# print(candidates)
+# c_vote = df['Candidate'].value_counts(normalize=True)
+# print(c_vote)
 
 import csv
 with open('PyPoll/Resources/election_data.csv', 'r') as f:
