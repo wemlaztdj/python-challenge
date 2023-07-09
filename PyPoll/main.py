e899c97825855db57155cd3a45cbfcf6d2760028
wemlaztdj <wemlhui@gmail.com>
2023-07-08 22:04:08 -0400

    Need more time on PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,23 +5,21 @@
 # The percentage of votes each candidate won
 # The total number of votes each candidate won
 # The winner of the election based on popular vote
-
-
-# import pandas as pd
-# df = pd.read_csv('PyPoll/Resources/election_data.csv')
-# total_votes = df['Ballot ID'].count()
-# candidates = df['Candidate'].unique()
-# print(total_votes)
-# print(candidates)
-# c_vote = df['Candidate'].value_counts(normalize=True)
-# print(c_vote)
-
+# In addition, your final script should both print the analysis to the terminal and export a text file with the results.
 import csv
+from collections import Counter
+
+total_vote =0
+candidates =[]
+
+
 with open('PyPoll/Resources/election_data.csv', 'r') as f:
   reader = csv.reader(f)
+  header = next(reader)
   for row in reader:
-    print(row)
-
+    total_vote +=1
+    candidates.append(row[2])
+vote = Counter(candidates)
 
 print('Election Results')
 print('-------------------------')
