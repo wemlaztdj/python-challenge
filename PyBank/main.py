7ffe35efc4037e6a9bf1db77ec73c4cf782d160f
wemlaztdj <wemlhui@gmail.com>
2023-07-08 20:36:31 -0400

    update to improt csv

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,17 +11,52 @@
 
 #In addition, your final script should both print the analysis to the terminal and export a text file with the results.
 
-import pandas as pd
+# import pandas as pd
 
-df = pd.read_csv('PyBank/Resources/budget_data.csv')
+# df = pd.read_csv('PyBank/Resources/budget_data.csv')
 
-total_months = df['Date'].count()
-net_total = df['Profit/Losses'].sum()
-max_total = df['Profit/Losses'].max()
-min_total = df['Profit/Losses'].min()
-avg_total = round(df['Profit/Losses'].mean(),2)
-max_date = df.loc[df['Profit/Losses'].idxmax(),'Date']
-min_date = df.loc[df['Profit/Losses'].idxmin(),'Date']
+# total_months = df['Date'].count()
+# net_total = df['Profit/Losses'].sum()
+# max_total = df['Profit/Losses'].max()
+# min_total = df['Profit/Losses'].min()
+# avg_total = round(df['Profit/Losses'].mean(),2)
+# max_date = df.loc[df['Profit/Losses'].idxmax(),'Date']
+# min_date = df.loc[df['Profit/Losses'].idxmin(),'Date']
+
+
+import csv
+total_months = 0
+net_total = 0
+max_total = 0
+min_total = 0
+max_date = ''
+min_date = ''
+with open('PyBank/Resources/budget_data.csv', 'r') as f:
+    reader = csv.reader(f)
+    header = next(reader)
+    # date_idx = header.index('Date')
+    # pl_idx = header.index('Profit/Losses')
+    # print(header)
+    # print(date_idx)
+    # print(pl_idx)
+    
+    for row in reader:
+        # print(row)
+        # print(row[0])
+        # print(row[1])
+        # print(int(row[1]))
+
+        net_total += int(row[1])
+        if int(row[1])> max_total:
+            max_total = int(row[1])
+            max_date = row[0]
+    
+        if int(row[1])< min_total or total_months == 0:
+            min_total = int(row[1])
+            min_date = row[0]
+        total_months += 1
+
+avg_total = round(net_total/total_months,2)
 
 #print to terminal
 print('Financial Analysis')
